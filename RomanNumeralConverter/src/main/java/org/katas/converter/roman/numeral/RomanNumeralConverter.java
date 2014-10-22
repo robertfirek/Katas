4b83ff9f8bd34e2699aad063d871ec14e40ac580
Robert Firek <robert.firek@gmail.com>
2014-10-22 01:20:05 +0100

    [RomanNumeralConverter] Refactoring - removed conversion tables, added Stream API

--- RomanNumeralConverter/src/main/java/org/katas/converter/roman/numeral/RomanNumeralConverter.java
+++ RomanNumeralConverter/src/main/java/org/katas/converter/roman/numeral/RomanNumeralConverter.java
@@ -1,79 +1,74 @@
 package org.katas.converter.roman.numeral;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class RomanNumeralConverter {
 
-    private final Map<Integer, String> units = new HashMap<Integer, String>() {{
-        put(1, "I");
-        put(2, "II");
-        put(3, "III");
-        put(4, "IV");
-        put(5, "V");
-        put(6, "VI");
-        put(7, "VII");
-        put(8, "VIII");
-        put(9, "IX");
-    }};
-
-    private final Map<Integer, String> tens = new HashMap<Integer, String>() {{
-        put(10, "X");
-        put(20, "XX");
-        put(30, "XXX");
-        put(40, "XL");
-        put(50, "L");
-        put(60, "LX");
-        put(70, "LXX");
-        put(80, "LXXX");
-        put(90, "XC");
-    }};
-
-    private final Map<Integer, String> hundreds = new HashMap<Integer, String>() {{
-        put(100, "C");
-        put(200, "CC");
-        put(300, "CCC");
-        put(400, "CD");
-        put(500, "D");
-        put(600, "DC");
-        put(700, "DCC");
-        put(800, "DCCC");
-        put(900, "CM");
-    }};
-
-    private final Map<Integer, String> thousands = new HashMap<Integer, String>() {{
-        put(1000, "M");
-        put(2000, "MM");
-        put(3000, "MMM");
-    }};
-
     public String convert(int number) {
-        int remainder = number;
-        int numberOfThousands = remainder / 1000;
+        RomanNumeralBuilder romanNumeralBuilder = new RomanNumeralBuilder(number);
 
-        remainder = number % 1000;
-        int numberOfHundreds = remainder / 100;
+        Optional<RomanNumeralBuilder> optionaRomanNumeralBuilder = Stream.<RomanNumeralBuilder>builder().add(romanNumeralBuilder).build()
+                .map(builder -> builder.appendRomanNumeral(getRomanNumeral(builder.getReminder() / 1000, "M", "M", "M"), builder.getReminder() % 1000))
+                .map(builder -> builder.appendRomanNumeral(getRomanNumeral(builder.getReminder() / 100, "C", "D", "M"), builder.getReminder() % 100))
+                .map(builder -> builder.appendRomanNumeral(getRomanNumeral(builder.getReminder() / 10, "X", "L", "C"), builder.getReminder() % 10))
+                .map(builder -> builder.appendRomanNumeral(getRomanNumeral(builder.getReminder(), "I", "V", "X"), builder.getReminder() % 1))
+                .findFirst();
 
-        remainder = remainder % 100;
-        int numberOfTens = remainder / 10;
+        return optionaRomanNumeralBuilder.get().getRomanNumeralString();
+    }
 
-        remainder = number % 10;
-        int numberOfUnits = remainder / 1;
-
-        StringBuilder romanNumeral = new StringBuilder();
-        if (numberOfThousands > 0) {
-            romanNumeral.append(this.thousands.get(numberOfThousands * 1000));
+    private String getRomanNumeral(int number, String prefix, String middle, String suffix) {
+        switch (number) {
+            case 1:
+            case 2:
+            case 3:
+                return repeatString(prefix, number);
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                return repeatString(prefix, number == 4 ? 1 : 0) + middle + repeatString(prefix, number > 5 ? number % 5 : 0);
+            case 9:
+                return prefix + suffix;
+            default:
+                return "";
         }
-        if (numberOfHundreds > 0) {
-            romanNumeral.append(this.hundreds.get(numberOfHundreds * 100));
+    }
+
+    private static String repeatString(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(s);
         }
-        if (numberOfTens > 0) {
-            romanNumeral.append(this.tens.get(numberOfTens * 10));
-        }
-        if (numberOfUnits > 0) {
-            romanNumeral.append(this.units.get(numberOfUnits));
+        return sb.toString();
+    }
+
+    private static class RomanNumeralBuilder {
+        private final String romanNumeralString;
+        private final int reminder;
+
+        public RomanNumeralBuilder(int reminder) {
+            this.romanNumeralString = "";
+            this.reminder = reminder;
         }
 
-        return romanNumeral.toString();
+        private RomanNumeralBuilder(String romanNumeralString, int reminder) {
+            this.romanNumeralString = romanNumeralString;
+            this.reminder = reminder;
+        }
+
+        public String getRomanNumeralString() {
+            return romanNumeralString;
+        }
+
+        public int getReminder() {
+            return reminder;
+        }
+
+        public RomanNumeralBuilder appendRomanNumeral(String romanNumeralString, int reminder) {
+            return new RomanNumeralBuilder(this.romanNumeralString + romanNumeralString, reminder);
+        }
     }
 }
